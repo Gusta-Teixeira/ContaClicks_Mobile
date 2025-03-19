a821a93a23bc93fbb3906667117aa7e76fd3cbae
Gustavo <gustavotsalgado@gmail.com>
2025-03-19 20:01:18 -0300

    Primeiro Commit

--- app/src/main/java/com/example/contaclick/MainActivity.java
+++ app/src/main/java/com/example/contaclick/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.contaclick;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button botao;
+    TextView wTexto;
+    Integer wNum=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        botao  = findViewById(R.id.btConta);
+        wTexto = findViewById(R.id.txTexto);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wNum++;
+                wTexto.setText(Integer.toString(wNum));
+            }
         });
     }
 }
